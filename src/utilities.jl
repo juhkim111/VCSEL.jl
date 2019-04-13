b1ebcfa3114ecdc3c5977f5c816624a44fbe144a
Juhyun Kim <juhkim111@ucla.edu>
2019-04-13 09:07:26 -0700

    change argument names

--- src/utilities.jl
+++ src/utilities.jl
@@ -1,52 +1,52 @@
 """
-    projectToNullSpace(yobs, Xobs, Vobs; tol=1e-6)
+    projectToNullSpace(y, X, V; tol=1e-6)
 
-Project `yobs` to null space of `Xobs` and transform `Vobs` accordingly.
+Project `y` to null space of `X` and transform `V` accordingly.
 
 # Input 
-- `yobs::Vector{Float64}`: response vector to be transformed. 
-- `Xobs::Matrix{Float64}`: covariate matrix whose null space `yobs` is projected to.
-- `Vobs::Vector{Matrix{Float64}}`: vector of covariance matrices to be transformed. 
+- `y::Vector{Float64}`: response vector to be transformed. 
+- `X::Matrix{Float64}`: covariate matrix whose null space `y` is projected to.
+- `V::Vector{Matrix{Float64}}`: vector of covariance matrices to be transformed. 
 - `tol::Float64`: if any diagonal entries  `Q` matrix in QR decomposition has its absolute
     value samller or equal to `tol`, then it is considered 0. That column is considered a
-    basis vector of null space of `I-Xobs(Xobs'Xobs)^{-1}Xobs'`. Default is 1e-6. 
+    basis vector of null space of `I-X(X'X)^{-1}X'`. Default is 1e-6. 
 
 # Ouptut 
-- `y`: projected response vector.
-- `V`: projected vector of covariance matrices. 
+- `ynew`: projected response vector.
+- `Vnew`: projected vector of covariance matrices. 
 
 """
 function projectToNullSpace(
-    yobs    :: Vector{Float64},
-    Xobs    :: Matrix{Float64},
-    Vobs    :: Vector{Matrix{Float64}};
-    tol     :: Float64 = 1e-6
-)
+    y    :: Vector{T},
+    X    :: Matrix{T},
+    V    :: Vector{Matrix{T}};
+    tol  :: T = 1e-6
+    ) where {T <: Float64}
 
     # number of groups 
-    m = length(Vobs) - 1
+    m = length(V) - 1
 
     ## REML: find B s.t. columns of B span the null space of X' and B'B = I
     # pivoted QR factorization of I-X(X'X)^{-1}X'
-    QRfact = qr(I - Xobs * inv(cholesky(Xobs' * Xobs)) * Xobs', Val(true))
+    QRfact = qr(I - X * inv(cholesky(X' * X)) * X', Val(true))
     # extract orthonormal basis of C(I-P)
     B = QRfact.Q[:, abs.(diag(QRfact.R)) .> tol] 
     # REML transformed response vector 
-    y = B' * yobs 
+    ynew = B' * y
     # REML transformed covariance matrices 
-    V  = Array{Matrix{Float64}}(undef, m + 1)
+    Vnew  = Array{Matrix{Float64}}(undef, m + 1)
     for i in 1:(m + 1)
-        V[i] = B' * Vobs[i] * B  
+        Vnew[i] = B' * V[i] * B  
     end  
 
     # make sure frobenius norm of Vi equals to 1 
     for i in 1:(m + 1)
-        if norm(V[i]) != 1
-            V[i] ./= norm(V[i])
+        if norm(Vnew[i]) != 1
+            Vnew[i] ./= norm(Vnew[i])
         end 
     end 
     
     # output 
-    return y, V 
+    return ynew, Vnew 
 
 end 
