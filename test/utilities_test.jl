16e57e6d4c805344bc3abf5af060c2283dc1cddd
Juhyun Kim <juhkim111@ucla.edu>
2019-04-18 04:20:51 -0700

    unit testing for utilities.jl

---
+++ test/utilities_test.jl
@@ -0,0 +1,43 @@
+module UtilitiesTest
+
+using Random, LinearAlgebra, Test, VarianceComponentSelect, PenaltyFunctions
+
+Random.seed!(123)
+
+# generate data from an univariate response variance component model 
+n = 100   # no. observations
+m = 10    # no. variance components
+p = 3     # no. covariates
+X = randn(n, p)
+β = ones(p)
+
+V  = Array{Matrix{Float64}}(undef, m + 1)
+for i = 1:m
+  Vi = randn(n, 50)
+  V[i] = Vi * Vi'
+  V[i] = V[i] ./ norm(V[i])
+end
+V[m + 1] = Matrix(I, n, n) ./ √n
+
+# truth 
+σ2 = zeros(m + 1)
+σ2[1] = σ2[4] = σ2[9] = 5.0
+σ2[end] = 1.0
+
+# form Ω
+Ω = zeros(n, n)
+for i = 1:(m + 1)
+  global Ω += σ2[i] * V[i]
+end
+Ωchol = cholesky(Ω)
+y = X * β + Ωchol.L * randn(n)
+
+ynew, Vnew, B = projectontonull(y, X, V)
+@test all(B'*X .≈ 0)
+for i in 1:(m + 1)
+    @test norm(Vnew[i]) ≈ 1
+end 
+
+
+
+end 
