65b4a20731cd359490aac8dfb5ddf49eac8991a6
Juhyun Kim <juhkim111@ucla.edu>
2019-04-18 10:39:28 -0700

    change to abstract type

--- src/maxlambda.jl
+++ src/maxlambda.jl
@@ -5,25 +5,27 @@
 
 Find the value of λ where all σ turns 0. At any value greater than λ, all σ's are 0.
 
-# Arguments
+# Input 
 - `y`: response vector
 - `V`: vector of covariance matrices
-- `penfun`: penalty function. 
-            Possible options are NoPenalty() (default), L1Penalty(), MCPPenalty().
-- `penwt`: weight vector
-- `maxiter`: maximum number of iterations. Default is 500. 
-- `tol`: value below which sigma is considered 0
+
+# Keyword 
+- `penfun`: penalty function, 
+      possible options are NoPenalty() (default), L1Penalty(), MCPPenalty()
+- `penwt`: vector of penalty weights, default is (1,1,...1,0)
+- `maxiter`: maximum number of iterations, default is 500
+- `tol`: value below which σ is considered 0
 
 # Output
-- `maxλ`: value of λ where all coeffecients turn zero (i.e. smallest λ with all zero coefficients)
+- `maxλ`: value of λ where all coeffecients turn zero, maximum λ on the solution path 
 """
 function maxlambda(
-    y       :: Vector{T},
-    V       :: Vector{Matrix{T}};
+    y       :: AbstractVector{T},
+    V       :: AbstractVector{AbstractMatrix{T}};
     penfun  :: Penalty = NoPenalty(),
-    penwt   :: Vector{T} = [ones(length(V)-1); 0.0],
+    penwt   :: AbstractVector{T} = [ones(T, length(V)-1); zero(T)],
     maxiter :: Int = 500,
-    tol     :: Float64 = 1e-8
+    tol     :: AbstractFloat = 1e-6
     ) where {T <: Real}
 
     # initialize values 
@@ -32,28 +34,29 @@ function maxlambda(
     m = length(V) - 1
 
     # initialize array 
-    λpath = Array{Float64}(undef, m)
+    λpath = Array{T}(undef, m)
+    Vy = similar(y)
 
-    # use derivative to find plausible lambda for each of `m`variance components 
+    # use derivative to find plausible lambda for each of `m` variance components 
     for i in eachindex(λpath)
-        Vy = V[i] * y
+        mul!(Vy, V[i], y) 
         λpath[i] = (1 / penwt[i]) * (-tr(V[i]) / (2 * σ02) +
                 dot(y, Vy) / (2 * σ02^2))
     end
 
-    # find maximum lambda among m different lambdas
+    # find maximum among m different lambdas
     tempλ = maximum(λpath)
-    sigmas = zeros(length(V))
+    σ2 = zeros(length(V))
     if tempλ <= 0
 	      tempλ = 30.0
     end
 
-    # make sure all estimated sigmas are 0 at current λ
+    # make sure all estimated σ2 are 0 at current λ
     while true
       # Step 1: obtain sigmas at tempλ
-      sigmas, = vcselect(y, V; penfun=penfun, λ=tempλ, penwt=penwt)
+      σ2, = vcselect(y, V; penfun=penfun, λ=tempλ, penwt=penwt)
       # Step 2: if all sigmas are zero, break the loop and move to Step 3.
-      all(sigmas[1:end-1] .< tol) && break;
+      all(σ2[1:end-1] .< tol) && break;
       # Step 2: else, multiply lambda by 1.5 and go to Step 1.
       tempλ = 1.5 * tempλ
     end
@@ -63,25 +66,25 @@ function maxlambda(
     a = 0.5 * tempλ
     b = tempλ
     c = 0.0
-    sigmas_a = Array{Float64}(undef, length(V))
-    sigmas_b = Array{Float64}(undef, length(V))
-    sigmas_c = Array{Float64}(undef, length(V))
+    σ2_a = Array{Float64}(undef, length(V))
+    σ2_b = Array{Float64}(undef, length(V))
+    σ2_c = Array{Float64}(undef, length(V))
 
     # loop through while iteration number less than maxiter 
     while iter <= maxiter
       c = (a + b) / 2
-      sigmas_a, = vcselect(y, V; penfun=penfun, λ=a, penwt=penwt)
-      sigmas_b, = vcselect(y, V; penfun=penfun, λ=b, penwt=penwt)
-      sigmas_c, = vcselect(y, V, penfun=penfun, λ=c, penwt=penwt)
+      σ2_a, = vcselect(y, V; penfun=penfun, λ=a, penwt=penwt)
+      σ2_b, = vcselect(y, V; penfun=penfun, λ=b, penwt=penwt)
+      σ2_c, = vcselect(y, V, penfun=penfun, λ=c, penwt=penwt)
 
-      if all(sigmas_a[1:end-1] .< tol)
+      if all(σ2_a[1:end-1] .< tol)
         b = a
         a = b / 2
-      # given that at least one sigma at a is non-zero, if difference between
-      #   sigmas at a and b are really small, break the loop
-      elseif maximum(abs, sigmas_b[1:end-1] - sigmas_a[1:end-1]) < tol || (b-a) < 0.01
+      # given that at least one σ2 at a is non-zero, if difference between
+      #   σ2 at a and b are really small, break the loop
+      elseif maximum(abs, σ2_b[1:end-1] - σ2_a[1:end-1]) < tol || (b-a) < 0.01
         break
-      elseif any(sigmas_c[1:end-1] .> tol)
+      elseif any(σ2_c[1:end-1] .> tol)
         a = c
       else
         b = c
