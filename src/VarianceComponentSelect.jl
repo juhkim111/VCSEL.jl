4a63fe837c44d12ef7ad9bb23fac10f1fd8e3ede
Juhyun Kim <juhkim111@ucla.edu>
2019-04-30 09:08:28 -0700

    export plot function

--- src/VarianceComponentSelect.jl
+++ src/VarianceComponentSelect.jl
@@ -4,8 +4,9 @@ using PenaltyFunctions
 using LinearAlgebra 
 using StatsBase
 using Distributions 
+using Plots 
 
-export vcselect, vcselectpath, maxlambda, nullprojection, betaestimate
+export vcselect, vcselectpath, maxlambda, nullprojection, betaestimate, plotsolpath
 
 include("vcselect.jl")
 include("maxlambda.jl")
