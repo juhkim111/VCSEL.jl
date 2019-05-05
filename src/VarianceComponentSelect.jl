899c0e5cace1718ef441c26cc35b75b9aab8edbd
Juhyun Kim <juhkim111@ucla.edu>
2019-05-05 12:21:54 -0700

    remove export

--- src/VarianceComponentSelect.jl
+++ src/VarianceComponentSelect.jl
@@ -6,9 +6,6 @@ using PenaltyFunctions, LinearAlgebra, StatsBase, Distributions, Reexport, Plots
 @reexport using PenaltyFunctions
 @reexport using Plots 
 
-export vcselect, vcselectpath, maxlambda, nullprojection, 
-        betaestimate, checkfrobnorm!, rankvarcomps, plotsolpath
-
 include("vcselect.jl")
 include("maxlambda.jl")
 include("utilities.jl")
