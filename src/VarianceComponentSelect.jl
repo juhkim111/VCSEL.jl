85c2d1c444ecddb39dce6b7cd7602455c77f24fe
Juhyun Kim <juhkim111@ucla.edu>
2019-05-05 14:09:21 -0700

    include multivariate vcselect

--- src/VarianceComponentSelect.jl
+++ src/VarianceComponentSelect.jl
@@ -7,7 +7,9 @@ using PenaltyFunctions, LinearAlgebra, StatsBase, Distributions, Reexport, Plots
 @reexport using Plots 
 
 include("vcselect.jl")
+include("vcselect_multivariate.jl")
 include("maxlambda.jl")
 include("utilities.jl")
+include("linalg_operations.jl")
 
 end # module
