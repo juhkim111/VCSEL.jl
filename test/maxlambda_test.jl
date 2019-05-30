cbe0d1efdaa81d77b1e7349b29a9304ae55ab3b2
Juhyun Kim <juhkim111@ucla.edu>
2019-05-29 20:32:34 -0700

    testing for multivariate response case

--- test/maxlambda_test.jl
+++ test/maxlambda_test.jl
@@ -1,10 +1,13 @@
 module MaxLambdaTest
 
-using Random, LinearAlgebra, VarianceComponentSelect, Test
-
+#using Random, LinearAlgebra, VarianceComponentSelect, Test
+include("../src/VarianceComponentSelect.jl") #
+using .VarianceComponentSelect #
+using Random, LinearAlgebra, Test #
 Random.seed!(123)
+tol = 1e-10
 
-# generate data from an univariate response variance component model 
+## generate data from an univariate response variance component model 
 n = 100   # no. observations
 m = 10    # no. variance components
 p = 3     # no. covariates
@@ -32,16 +35,55 @@ end
 Ωchol = cholesky(Symmetric(Ω))
 y = Ωchol.L * randn(n)
 
-maxλ_lasso = maxlambda(y, V; penfun=L1Penalty())
-maxλ_mcp = maxlambda(y, V; penfun=MCPPenalty())
-σ2_lasso, = vcselect(y, V; penfun=L1Penalty(), λ=maxλ_lasso) 
-σ2_mcp, = vcselect(y, V; penfun=MCPPenalty(), λ=maxλ_mcp) 
+@time maxλ_lasso = maxlambda(y, V; penfun=L1Penalty())
+@time maxλ_mcp = maxlambda(y, V; penfun=MCPPenalty())
+σ̂2_lasso, = vcselect(y, V; penfun=L1Penalty(), λ=maxλ_lasso) 
+σ̂2_mcp, = vcselect(y, V; penfun=MCPPenalty(), λ=maxλ_mcp) 
 
-tol = 1e-6
+tol = 1e-10
 @testset  begin 
-    @test all(σ2_lasso[:, 1:end-1] .> tol)
-    @test all(σ2_mcp[:, 1:end-1] .> tol)
+    @test all(σ̂2_lasso[:, 1:end-1] .> tol)
+    @test all(σ̂2_mcp[:, 1:end-1] .> tol)
 end 
 
+## generate data from a d-variate response variance component model
+n = 100         # no. observations
+d = 3           # no. categories
+nvarcomps = 6   # no. variance components
+p = 4           # no. covariates
+X = randn(n, p) # covariate matrix 
+β = ones(p, d)  # fixed effects parameter matrix 
+
+# variance component matrix 
+Σ = [zeros(d, d) for i in 1:nvarcomps]
+for i in [1, 4, 6]
+  Σi = randn(d, d)
+  Σ[i] = Σi * Σi'
+end
+
+# vector of covariance matrix 
+V  = Array{Matrix{Float64}}(undef, nvarcomps)
+for i = 1:(nvarcomps - 1)
+  Vi = randn(n, 50)
+  V[i] =  Vi * Vi'
+end
+V[end] = Matrix(I, n, n)
+
+# form Ω
+Ω = zeros(n*d, n*d)
+for i = 1:nvarcomps
+    Ω .+= kron(Σ[i], V[i])
+end
+Ωchol = cholesky!(Symmetric(Ω))
+
+# generate response vector (no covariate matrix)
+Y = reshape(Ωchol.L * randn(n*d), n, d)
+@time maxλ_lasso, iter = maxlambda(Y, V; penfun=L1Penalty())
+Σ̂_lasso, = vcselect(Y, V; penfun=L1Penalty(), λ=maxλ_lasso) 
+@testset begin 
+    for i in 1:(nvarcomps - 1)
+      @test isapprox(Σ̂_lasso[i], zeros(d, d); atol=tol)
+    end 
+end 
 
 end 
