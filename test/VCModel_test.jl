3f682ab4aa08fcfa4c24d0edefd5dc176c74b7b5
Juhyun Kim <juhkim111@ucla.edu>
2019-07-16 07:21:28 -0400

    testing for struct

--- test/VCModel_test.jl
+++ test/VCModel_test.jl
@@ -4,39 +4,43 @@ using Random, LinearAlgebra, Test
 include("../src/VarianceComponentSelect.jl")
 using .VarianceComponentSelect
 
-# Random.seed!(123)
-# n, m, p = 100, 6, 4
-# X = randn(n, p)
-# β = ones(p)
-# V  = Array{Matrix{Float64}}(undef, m + 1)
-# for i = 1:m
-#   Vi = randn(n, 50)
-#   #V[i] = Vi * Vi'
-#   V[i] = Vi * Vi'
-#   V[i] = V[i] ./ norm(V[i])
-# end
-# V[m + 1] = Matrix(I, n, n) ./ √n
+Random.seed!(123)
+n, m, p = 100, 6, 3
+X = randn(n, p)
+β = ones(p)
+V  = Array{Matrix{Float64}}(undef, m + 1)
+for i = 1:m
+  Vi = randn(n, 50)
+  #V[i] = Vi * Vi'
+  V[i] = Vi * Vi'
+  V[i] = V[i] ./ norm(V[i])
+end
+V[m + 1] = Matrix(I, n, n) ./ √n
 
-# # truth 
-# σ2 = zeros(m + 1)
-# σ2[1] = σ2[3] = 5.0
-# σ2[end] = 1.0
+# truth 
+σ2 = zeros(m + 1)
+σ2[1] = σ2[3] = 5.0
+σ2[end] = 1.0
 
-# # form Ω
-# Ω = zeros(n, n)
-# for i = 1:(m + 1)
-#    #Ω .+= σ2[i] * V[i]
-#    axpy!(σ2[i], V[i], Ω)
-# end
-# Ωchol = cholesky(Symmetric(Ω))
-# y1 = Ωchol.L * randn(n)
-# y2 = X * β + Ωchol.L * randn(n)
+# form Ω
+Ω = zeros(n, n)
+for i = 1:(m + 1)
+   #Ω .+= σ2[i] * V[i]
+   axpy!(σ2[i], V[i], Ω)
+end
+Ωchol = cholesky(Symmetric(Ω))
+yreml = Ωchol.L * randn(n)
+y = X * β + Ωchol.L * randn(n)
 
-# #vcm1 = VCModel(y1, V, [0.5, 0.5, 0.5, 0.5, 0.5, 1.0])
-# vcm2 = VCModel(y2, X, V, [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1.0])
+vcm1 = VCModel(yreml, V, [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1.0])
+vcm2 = VCModel(y, X, V, [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1.0])
 
-# Σpath, βpath, λpath, objpath, niterspath = vcselectpath!(vcm2; penfun=L1Penalty())
 
+# Σ̂path, β̂path, λpath, objpath, niterspath = vcselectpath!(vcm2; penfun=L1Penalty())
+# @testset begin 
+#   @test vcm2.Σ == Σ̂path[:, end]
+
+# end 
 
 # @info "OGOG"
 # sigma2path, objpath, λpath, _, betapath = vcselectpath(y2, X, V; σ2=[0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1.0],
@@ -73,7 +77,7 @@ using .VarianceComponentSelect
 
 ## generate data from a d-variate (d>1) response variance component model
 Random.seed!(123)
-n = 100         # no. observations
+n = 50         # no. observations
 d = 3           # no. categories
 nvarcomps = 5   # no. variance components
 p = 4           # no. covariates
@@ -91,7 +95,7 @@ end
 # vector of covariance matrix 
 V  = Array{Matrix{Float64}}(undef, nvarcomps)
 for i = 1:(nvarcomps - 1)
-  Vi = randn(n, 50)
+  Vi = randn(n, 20)
   V[i] = Vi * Vi'
   V[i] = V[i] ./ norm(V[i])
 end
@@ -111,51 +115,63 @@ Y = X * β + reshape(Ωchol.L * randn(n*d), n, d)
 # define VCModel
 Σinit = [ones(d, d) for i in 1:nvarcomps]
 Σinit[end] = Matrix(I, d, d)
-#vcmreml = VCModel(Yreml, V, Σinit)
-vcm = VCModel(Y, X, V, Σinit)
-
-
-
-# vcselect!(vcm; penfun=L1Penalty(), λ=25.0)
-# Σinit = [ones(d, d) for i in 1:nvarcomps]
-# Σinit[end] = Matrix(I, d, d)
-# Σ̂, β, = vcselect(Y, X, V; penfun=L1Penalty(), λ=25.0, Σ=Σinit)
-# println("vcm.Σ = ", vcm.Σ)
-# println("Σ̂ = ", Σ̂)
-# println("vcm.β = ", vcm.β)
-# println("β =", β)
-# @testset begin
-# @test Σ̂ == vcmreml.Σ
-# end
-
-nlambda = 2 
-
-
-@info "vcselectpath OG"
-
+vcmreml_mult = VCModel(Yreml, V, Σinit)
 Σinit = [ones(d, d) for i in 1:nvarcomps]
 Σinit[end] = Matrix(I, d, d)
-@time Σpath_og, objpath_og, lambdapath_og, _, betapath_og   = vcselectpath(Y, X, V;
-      penfun=L1Penalty(), nlambda=nlambda, Σ=Σinit, fixedeffects=true, verbose=true)
-# # 
-
-@info "vcselectpath!"
+vcm_mult = VCModel(Y, X, V, Σinit)
 
 
-@time Σ̂path, betapath, lambdapath, objpath,  = vcselectpath!(vcm; penfun=L1Penalty(), 
-  nλ=nlambda, verbose=true)
+
+# # Σ̂, β̂, obj, niters, Ω̂, objvec = vcselect!(vcm; penfun=L1Penalty(), λ=5.0, verbose=true)
+
+# # @testset begin 
+# #   for i in 1:(length(objvec) - 1)
+# #     @test objvec[i] >= objvec[i+1]
+# #   end 
+# # end 
+
+# # Σinit = [ones(d, d) for i in 1:nvarcomps]
+# # Σinit[end] = Matrix(I, d, d)
+# # Σ̂, β, = vcselect(Y, X, V; penfun=L1Penalty(), λ=25.0, Σ=Σinit)
+# # println("vcm.Σ = ", vcm.Σ)
+# # println("Σ̂ = ", Σ̂)
+# # println("vcm.β = ", vcm.β)
+# # println("β =", β)
+# # @testset begin
+# # @test Σ̂ == vcmreml.Σ
+# # end
+
+# nlambda = 2 
 
 
-# Σpath_og, objpath_og, lambdapath_og,   = vcselectpath(Yreml, V;
-#   penfun=L1Penalty(), nlambda=nlambda, Σ=Σinit, verbose=true)
-# # 
+# @info "vcselectpath OG"
 
-println("objpath = ", objpath)
-println("objpath_og =", objpath_og)
-println("Σpath_og = ", Σpath_og)
-println("Σpath = ", Σ̂path)
-println("betapath = ", betapath)
-println("betapath_og=", betapath_og)
+# Σinit = [ones(d, d) for i in 1:nvarcomps]
+# Σinit[end] = Matrix(I, d, d)
+# @time Σpath_og, objpath_og, lambdapath_og, _, betapath_og   = vcselectpath(Y, X, V;
+#       penfun=L1Penalty(), nlambda=nlambda, Σ=Σinit, fixedeffects=true, verbose=true)
+# # # 
+
+# # @info "vcselectpath!"
+
+# @info "vcselectpath!"
+# @time Σ̂path, betapath, lambdapath, objpath,  = vcselectpath!(vcm; penfun=L1Penalty(), 
+#   nλ=nlambda, verbose=true)
+
+# # Σinit = [ones(d, d) for i in 1:nvarcomps]
+# # Σinit[end] = Matrix(I, d, d)
+
+# # @info "vcselectpath OG"
+# # @time Σpath_og, objpath_og, lambdapath_og,   = vcselectpath(Yreml, V;
+# #   penfun=L1Penalty(), nlambda=nlambda, Σ=Σinit, verbose=true)
+
+
+# println("objpath = ", objpath)
+# println("objpath_og =", objpath_og)
+# println("Σpath_og = ", Σpath_og)
+# println("Σpath = ", Σ̂path)
+# println("betapath = ", betapath)
+# println("betapath_og=", betapath_og)
 
 
 
--- test/vcselect_univariate_test.jl
+++ test/vcselect_univariate_test.jl
@@ -1,12 +1,14 @@
 module UnivariateTest
 
-using Random, LinearAlgebra, VarianceComponentSelect, Test
+#using Random, LinearAlgebra, VarianceComponentSelect, Test
+include("../src/VarianceComponentSelect.jl")
+using Random, LinearAlgebra, .VarianceComponentSelect, Test
 
 Random.seed!(123)
 
 # generate data from an univariate response variance component model 
 n = 100   # no. observations
-m = 10    # no. variance components
+m = 6     # no. variance components
 p = 3     # no. covariates
 X = randn(n, p)
 β = ones(p)
@@ -22,7 +24,7 @@ V[m + 1] = Matrix(I, n, n) ./ √n
 
 # truth 
 σ2 = zeros(m + 1)
-σ2[1] = σ2[4] = σ2[9] = 5.0
+σ2[1] = σ2[4] = 5.0
 σ2[end] = 1.0
 
 # form Ω
@@ -36,32 +38,78 @@ yreml = Ωchol.L * randn(n)
 y = X * β + Ωchol.L * randn(n)
 nlambda = 20 
 
-@info "check if objective values are monotonically decreasing (no penalty)" 
-temp, _, _, _, objvec = vcselect(y, V; verbose=true)
-penwt = zeros(m + 1)
-penwt[1:m] = 1 ./ sqrt.(temp[1:m])
+vcm1 = VCModel(yreml, V, [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1.0])
+vcm2 = VCModel(y, X, V, [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1.0])
+vcm22 = VCModel(y, X, V, [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1.0])
+
+@info "objective values are monotonically decreasing (no penalty)" 
+Σ̂1, β̂1, obj1, niters1, Ω̂1, objvec = vcselect!(vcm1; verbose=true)
 @testset begin 
   for i in 1:(length(objvec) - 1)
     @test objvec[i] >= objvec[i+1]
   end 
 end 
 
-@info "check if objective values are monotonically decreasing (L1 penalty)" 
-σ2, _, _, _, objvec = vcselect(y, V; penfun=L1Penalty(), λ=15.0, verbose=true,
-      penwt = penwt)
+Σ̂2, β̂2, obj2, niters2, Ω̂2, objvec = vcselect!(vcm2; verbose=true)
 @testset begin 
   for i in 1:(length(objvec) - 1)
     @test objvec[i] >= objvec[i+1]
   end 
 end 
 
-@info "check if objective values are monotonically decreasing (MCP penalty)" 
-σ2, _, _, _, objvec = vcselect(y, V; penfun=MCPPenalty(), λ=22.0, verbose=true)
+@info "objective values are monotonically decreasing (L1 Penalty)"
+Σ̂22, β̂22, obj22, niters22, _, objvec22 = vcselect!(vcm22; 
+      penfun=L1Penalty(), λ=2.0, verbose=true)
 @testset begin 
-  for i in 1:(length(objvec) - 1)
-    @test objvec[i] >= objvec[i+1]
+  for i in 1:(length(objvec22) - 1)
+    @test objvec22[i] >= objvec22[i+1]
   end 
 end 
+
+# reset and test if the same wth the previous 
+resetVCModel!(vcm2, [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 1.0])
+Σ̂_L1, β̂_L1, obj_L1, niters_L1, _, objvec_L1 = vcselect!(vcm2; 
+        penfun=L1Penalty(), λ=2.0, verbose=true)
+@testset begin 
+  for i in 1:(length(objvec_L1) - 1)
+    @test objvec_L1[i] >= objvec_L1[i+1]
+  end 
+end 
+
+@info "check if resetVCModel! works"
+@testset begin 
+  @test Σ̂22 == Σ̂_L1 
+  @test β̂22 == β̂_L1
+  @test obj22 == obj_L1
+  @test niters22 == niters_L1
+end 
+
+# @info "check if objective values are monotonically decreasing (no penalty)" 
+# temp, _, _, _, objvec = vcselect(y, V; verbose=true)
+# penwt = zeros(m + 1)
+# penwt[1:m] = 1 ./ sqrt.(temp[1:m])
+# @testset begin 
+#   for i in 1:(length(objvec) - 1)
+#     @test objvec[i] >= objvec[i+1]
+#   end 
+# end 
+
+# @info "check if objective values are monotonically decreasing (L1 penalty)" 
+# σ2, _, _, _, objvec = vcselect(y, V; penfun=L1Penalty(), λ=15.0, verbose=true,
+#       penwt = penwt)
+# @testset begin 
+#   for i in 1:(length(objvec) - 1)
+#     @test objvec[i] >= objvec[i+1]
+#   end 
+# end 
+
+# @info "check if objective values are monotonically decreasing (MCP penalty)" 
+# σ2, _, _, _, objvec = vcselect(y, V; penfun=MCPPenalty(), λ=22.0, verbose=true)
+# @testset begin 
+#   for i in 1:(length(objvec) - 1)
+#     @test objvec[i] >= objvec[i+1]
+#   end 
+# end 
 
 # ## variance component selection at specific lambda 
 # σ2_tmp, = vcselect(y, V)
