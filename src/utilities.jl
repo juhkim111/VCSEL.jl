c1ae59b2d01ea8e3a69c57c1504e5daa0b1d6bba
Juhyun Kim <juhkim111@ucla.edu>
2019-07-31 16:37:00 -0400

    remove checkfrobnorm! function

--- src/utilities.jl
+++ src/utilities.jl
@@ -1,22 +1,22 @@
-export plotsolpath, checkfrobnorm!, rankvarcomps, matarray2mat
+export plotsolpath, rankvarcomps, matarray2mat
 
-"""
-    checkfrobnorm!(V)
+# """
+#     checkfrobnorm!(V)
 
-Check if frobenius norm of Vi in V equals to 1. If not, divide by its norm. 
-"""
-function checkfrobnorm!(
-    V :: AbstractVector{Matrix{T}}
-) where {T <: Real}
+# Check if frobenius norm of Vi in V equals to 1. If not, divide by its norm. 
+# """
+# function checkfrobnorm!(
+#     V :: AbstractVector{Matrix{T}}
+# ) where {T <: Real}
 
-    frobnorm = 0
-    for Vi in V
-        frobnorm = norm(Vi) 
-        if frobnorm != 1
-            Vi ./= frobnorm
-        end
-    end 
-end 
+#     frobnorm = 0
+#     for Vi in V
+#         frobnorm = norm(Vi) 
+#         if frobnorm != 1
+#             Vi ./= frobnorm
+#         end
+#     end 
+# end 
 """
     rankvarcomps(σ2path; tol=1e-6)
 
