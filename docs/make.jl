aca39d8a56035ee2a351f10049d6aa1a8c04648b
Juhyun Kim <juhkim111@ucla.edu>
2020-02-25 12:44:12 -0800

    updated pages

--- docs/make.jl
+++ docs/make.jl
@@ -1,14 +1,22 @@
-push!(LOAD_PATH,"../src/")
+push!(LOAD_PATH, "../src/")
 using Documenter, VCSEL
 
 makedocs(
+    doctest = false, 
     format = Documenter.HTML(),
     sitename = "VCSEL.jl",
     authors = "Juhyun Kim",
+    clean = true, 
     pages = [
         "Home" => "index.md",
-        #"Examples" => , 
-        "API" => "man/api.md",
+        "VCModel" => "man/VCModel.md", 
+        # "Example: VCModel" => "man/VCModel_example.md",
+        # "Example: VCintModel" => "man/VCintModel_example.md"
+        "Examples" => Any[
+            "man/VCModel_example.md",
+            "man/VCintModel_example.md"
+        ],
+        #"API" => "man/api.md",
     ]
 )
 
