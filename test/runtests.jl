21fb59d760c334eb1ce4b3a4837dd50cbeeaef46
Juhyun Kim <juhkim111@ucla.edu>
2020-03-02 10:46:38 -0800

    add test for multivariate

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 module vcselect_univariate_test
   include("vcselect_univariate_test.jl")
-  #include("vcselect_multivariate_test.jl)
+  include("vcselect_multivariate_test.jl)
   include("vcselect_interact_test.jl")
   include("maxlambda_test.jl")
   include("utilities_test.jl")
