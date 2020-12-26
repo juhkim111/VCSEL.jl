c4dafeb7e967e2d9bd5c20d0ed6c95279cac53ce
Juhyun Kim <juhkim111@ucla.edu>
2020-12-26 14:04:02 -0800

    input G, not V

--- src/maxlambda.jl
+++ src/maxlambda.jl
@@ -1,5 +1,3 @@
-export maxlambda 
-
 """
 
     maxlambda(y, V; penfun=NoPenalty(), penwt=[ones(length(V)-1); 0.0], maxiters=500, 
@@ -107,10 +105,10 @@ function maxlambda(
 end
 
 """
-    maxlambda(Y, V; penfun=NoPenalty(), penwt=[ones(length(V)-1); 0.0], standardize=false,
+    findmaxλ(Y, G; penfun=NoPenalty(), penwt=[ones(length(V)-1); 0.0], 
               maxiters=500, tol=1e-8)
 
-Find the value of λ where all σ turns 0. At any value greater than λ, all σ's are 0.
+Find the value of λ where all Σ turns 0. At any value greater than λ, all Σ's are 0.
 
 # Input 
 - `Y`: response matrix
@@ -129,15 +127,17 @@ Find the value of λ where all σ turns 0. At any value greater than λ, all σ'
 # Output
 - `maxλ`: value of λ where all coeffecients turn zero, maximum λ on the solution path 
 """
-function maxlambda(
-    Y       :: AbstractMatrix{T},
-    V       :: AbstractVector{Matrix{T}};
+function findmaxλ(
+    Y       :: AbstractVecOrMat{T},
+    G       :: AbstractVector{Matrix{T}};
     penfun  :: Penalty = NoPenalty(),
-    penwt   :: AbstractVector{T} = [ones(T, length(V)-1); zero(T)],
-    standardize :: Bool = false, 
+    penwt   :: AbstractVector{T} = [ones(T, length(G)-1); zero(T)],
+    #standardize :: Bool = false, 
     maxiters :: Int = 500,
     tol     :: AbstractFloat = 1e-8,
-    tempλ   :: AbstractFloat = 50.0
+    tempλ   :: AbstractFloat = 50.0,
+    Mdxd    :: AbstractMatrix{T} = Matrix{T}(undef, size(Y, 2), size(Y, 2)),
+    Mnxd    :: AbstractMatrix{T} = Matrix{T}(undef, size(Y, 1), size(Y, 2))
     ) where {T <: Real}
 
     # if no penalty, return 0 
@@ -147,58 +147,58 @@ function maxlambda(
 
     # initialize values 
     n, d = size(Y)
-    nvarcomps = length(V) 
-    m = length(V) - 1
+    m = length(G) 
 
     # 
-    YtY = BLAS.gemm('T', 'N', Y, Y) # Yt * Y 
-    cholYtY = cholesky!(Symmetric(YtY))          
-    YtYinv = inv(cholYtY)
-    W = Y * YtYinv                  # Y * inv(Yt * Y)
-    const1 = -n * tr(YtYinv)
-    n2 = n^2
+    BLAS.syrk!('U', 'T', true, Y, false, Mdxd)
+    chol = cholesky!(Symmetric(Mdxd))          
+    cholinv = inv(chol)
+    const1 = -n * tr(cholinv)
+    mul!(Mnxd, Y, cholinv) # n * d
 
     # initialize array 
-    λpath = Array{Float64}(undef, m)
+    potentialλs = Array{T}(undef, m)
 
     # use derivative to find plausible lambda for each of `m` variance components 
-    for i in eachindex(λpath)
-        λpath[i] =  const1 * tr(V[i]) + 
-                n2 * tr(BLAS.gemm('T', 'N', W, V[i] * W))
+    for i in eachindex(potentialλs)
+      BLAS.syrk!('U', 'T', true, G[i]' * Mnxd, false, Mdxd)
+      potentialλs[i] =  const1 * tr(G[i] * G[i]') + n^2 * tr(Mdxd)
     end
+    println("potentialλs=", potentialλs)
 
     # find maximum among m different lambdas
-    tempλ = maximum(λpath)
-    σ2 = zeros(length(V))
+    tempλ = maximum(potentialλs)
     if tempλ <= 0
 	      tempλ = 30.0
     end
+    println("tempλ=", tempλ)
 
     # make sure all estimated σ2 are 0 at current λ
     while true
       # Step 1: obtain sigmas at tempλ
-      Σ, = vcselect(Y, V; penfun=penfun, λ=tempλ, penwt=penwt, standardize=standardize)
+      Σ, = vcselect(Y, G; penfun=penfun, λ=tempλ, penwt=penwt)
       # Step 2: if all sigmas are zero, break the loop and move to Step 3.
       maximum(tr.(view(Σ, 1:m))) < tol && break;
       # Step 2: else, multiply lambda by 1.5 and go to Step 1.
       tempλ = 1.5 * tempλ
     end
+    println("tempλ=", tempλ)
 
     # Step 3: Use bisection method
     iter = 1
     a = 0.5 * tempλ
     b = tempλ
     c = 0.0
-    Σ_a = Vector{Matrix{Float64}}(undef, nvarcomps)
-    Σ_b = Vector{Matrix{Float64}}(undef, nvarcomps)
-    Σ_c = Vector{Matrix{Float64}}(undef, nvarcomps)
+    Σ_a = Vector{Matrix{T}}(undef, nvarcomps)
+    Σ_b = Vector{Matrix{T}}(undef, nvarcomps)
+    Σ_c = Vector{Matrix{T}}(undef, nvarcomps)
 
     # loop through while iteration number less than maxiters
     while iter <= maxiters
       c = (a + b) / 2
-      Σ_a, = vcselect(Y, V; penfun=penfun, λ=a, penwt=penwt, standardize=standardize)
-      Σ_b, = vcselect(Y, V; penfun=penfun, λ=b, penwt=penwt, standardize=standardize)
-      Σ_c, = vcselect(Y, V, penfun=penfun, λ=c, penwt=penwt, standardize=standardize)
+      Σ_a, = vcselect(Y, G; penfun=penfun, λ=a, penwt=penwt)
+      Σ_b, = vcselect(Y, G; penfun=penfun, λ=b, penwt=penwt)
+      Σ_c, = vcselect(Y, G; penfun=penfun, λ=c, penwt=penwt)
 
       if maximum(tr.(view(Σ_a, 1:m))) < tol 
         b = a
@@ -220,17 +220,16 @@ function maxlambda(
     return maxλ, iter
 end
 
-"""
 
-    maxlambda(y, V, Vint; penfun=NoPenalty(), penwt=[ones(length(V)-1); 0.0], 
+
+"""
+    findmaxλ(vcm; penfun=NoPenalty(), penwt=[ones(length(V)-1); 0.0], 
               maxiters=500, tol=1e-8)
 
-Find the value of λ where all σ turns 0. At any value greater than λ, all σ's are 0.
+Find the value of λ where all Σ turns 0. At any value greater than λ, all Σ's are 0.
 
 # Input 
-- `y`: response vector
-- `V`: vector of covariance matrices
-- `Vint`: vector of covariance matrices for interaction 
+- `vcm`: VCModel 
 
 # Keyword 
 - `penfun`: penalty function, possible options are `L1Penalty()` (default), `MCPPenalty()`, 
@@ -243,18 +242,22 @@ Find the value of λ where all σ turns 0. At any value greater than λ, all σ'
 - `tol`: value below which estimate is considered zero
 
 # Output
-- `maxλ`: value of `λ` where all coeffecients turn zero, maximum λ on the solution path 
+- `maxλ`: value of λ where all coeffecients turn zero, maximum λ on the solution path 
 """
-function maxlambda(
-    y           :: AbstractVector{T},
-    V           :: AbstractVector{Matrix{T}},
-    Vint        :: AbstractVector{Matrix{T}};
-    penfun      :: Penalty = NoPenalty(),
-    penwt       :: AbstractVector{T} = [ones(T, length(Vint)); zero(T)],
-    standardize :: Bool = false, 
-    maxiters    :: Int = 500,
-    tol         :: AbstractFloat = 1e-8
+function findmaxλ(
+    vcm     :: VCModel{T};
+    penfun  :: Penalty = NoPenalty(),
+    penwt   :: AbstractVector{T} = [ones(T, nvarcomps(vcm)-1); zero(T)],
+    #standardize :: Bool = false, 
+    maxiters :: Int = 500,
+    tol     :: AbstractFloat = 1e-8,
+    tempλ   :: AbstractFloat = 50.0
     ) where {T <: Real}
+
+    # 
+    vcmtmp = deepcopy(vcm)
+    Σinit = deepcopy(vcm.Σ)
+    println("Σinit =", Σinit)
 
     # if no penalty, return 0 
     if isa(penfun, NoPenalty)
@@ -262,73 +265,80 @@ function maxlambda(
     end 
 
     # initialize values 
-    n = length(y)
-    σ2_0 = dot(y, y) / n
-    m = length(V) - 1
+    n, d = size(vcmtmp)[1], size(vcmtmp)[2]
+    m = nvarcomps(vcmtmp) - 1 
+
+    # 
+    BLAS.syrk!('U', 'T', true, reshape(vcmtmp.vecY, n, d), false, vcmtmp.Mdxd)
+    chol = cholesky!(Symmetric(vcmtmp.Mdxd))          
+    vcmtmp.Mdxd[:] = inv(chol)
+    const1 = -n * tr(vcmtmp.Mdxd)
+    mul!(vcmtmp.Mnxd, reshape(vcmtmp.vecY, n, d), vcmtmp.Mdxd) # n * d
 
     # initialize array 
-    λpath = Array{Float64}(undef, m)
-    λpath_int = Array{Float64}(undef, m)
-    Vy = similar(y)
+    potentialλs = Array{T}(undef, m)
 
     # use derivative to find plausible lambda for each of `m` variance components 
-    for i in 1:m
-      mul!(Vy, V[i], y) 
-      λpath[i] = (1 / penwt[i]) * (-tr(V[i]) / σ2_0 + dot(y, Vy) / σ2_0^2)
-      mul!(Vy, Vint[i], y) 
-      λpath_int[i] = (1 / penwt[i]) * (-tr(Vint[i]) / σ2_0 + dot(y, Vy) / σ2_0^2)
+    for i in eachindex(potentialλs)
+      BLAS.syrk!('U', 'T', true, vcmtmp.G[i]' * vcmtmp.Mnxd, false, vcmtmp.Mdxd)
+      potentialλs[i] =  const1 * tr(vcmtmp.G[i] * vcmtmp.G[i]') + n^2 * tr(vcmtmp.Mdxd)
     end
+    println("potentialλs=", potentialλs)
 
     # find maximum among m different lambdas
-    tempλ = abs(minimum([λpath; λpath_int]))
-    if isapprox(tempλ, 0; atol=1e-8)
-      tempλ = maximum(λpath)
-    end 
-
-    σ2 = zeros(Float64, m+1)
-    σ2int = zeros(Float64, m)
+    tempλ = maximum(potentialλs)
+    if tempλ <= 0
+	      tempλ = 30.0
+    end
 
     # make sure all estimated σ2 are 0 at current λ
     while true
       # Step 1: obtain sigmas at tempλ
-      σ2, σ2int, = vcselect(y, V, Vint; penfun=penfun, λ=tempλ, penwt=penwt, 
-                  standardize=standardize)
+      vcselect!(vcmtmp; penfun=penfun, λ=tempλ, penwt=penwt)
       # Step 2: if all sigmas are zero, break the loop and move to Step 3.
-      maximum(view(σ2, 1:m)) < tol && maximum(σ2int) < tol && break;
+      maximum(tr.(view(vcmtmp.Σ, 1:m))) < tol && break;
+      resetModel!(vcmtmp, Σinit)
       # Step 2: else, multiply lambda by 1.5 and go to Step 1.
       tempλ = 1.5 * tempλ
     end
 
+    println("tempλ=", tempλ)
+
     # Step 3: Use bisection method
+    resetModel!(vcmtmp, Σinit)
     iter = 1
     a = 0.5 * tempλ
     b = tempλ
     c = 0.0
-    σ2_a = similar(σ2)
-    σ2_b = similar(σ2)
-    σ2_c = similar(σ2)
-    σ2int_a = similar(σ2int)
-    σ2int_b = similar(σ2int)
-    σ2int_c = similar(σ2int)
+    Σ_a = deepcopy(Σinit) 
+    Σ_b = deepcopy(Σinit) 
+    Σ_c = deepcopy(Σinit) 
 
-    # loop through while iteration number less than maxiter 
+    # loop through while iteration number less than maxiters
     while iter <= maxiters
       c = (a + b) / 2
-      σ2_a, σ2int_a, = vcselect(y, V, Vint; penfun=penfun, λ=a, penwt=penwt,
-                      standardize=standardize)
-      σ2_b, σ2int_b, = vcselect(y, V, Vint; penfun=penfun, λ=b, penwt=penwt, 
-                      standardize=standardize)
-      σ2_c, σ2int_c, = vcselect(y, V, Vint; penfun=penfun, λ=c, penwt=penwt, 
-                      standardize=standardize)
 
-      if maximum(view(σ2_a, 1:m)) < tol && maximum(σ2int_a) < tol 
+      vcselect!(vcmtmp; penfun=penfun, λ=a, penwt=penwt, checktype=false)
+      copyto!(Σ_a, vcmtmp.Σ)
+      resetModel!(vcmtmp, Σinit)
+      #println("vcmtmp.Σ=", vcmtmp.Σ)
+      vcselect!(vcmtmp; penfun=penfun, λ=b, penwt=penwt, checktype=false)
+      copyto!(Σ_b, vcmtmp.Σ)
+      resetModel!(vcmtmp, Σinit)
+      #println("vcmtmp.Σ=", vcmtmp.Σ)
+      vcselect!(vcmtmp; penfun=penfun, λ=c, penwt=penwt, checktype=false)
+      copyto!(Σ_c, vcmtmp.Σ)
+      resetModel!(vcmtmp, Σinit)
+      #println("vcmtmp.Σ=", vcmtmp.Σ)
+
+      if maximum(tr.(view(Σ_a, 1:m))) < tol 
         b = a
         a = b / 2
       # given that at least one σ2 at a is non-zero, if difference between
       #   σ2 at a and b are really small, break the loop
-      elseif maximum(abs, view(σ2_b, 1:m) - view(σ2_a, 1:m)) < tol || (b-a) < 0.01
+      elseif maximum(abs, tr.(view(Σ_b, 1:m)) - tr.(view(Σ_a, 1:m))) < tol || (b-a) < 0.01
         break
-      elseif maximum(view(σ2_c, 1:m)) > tol && maximum(σ2int_c) > tol
+      elseif maximum(tr.(view(Σ_c, 1:m))) > tol 
         a = c
       else
         b = c
@@ -337,6 +347,7 @@ function maxlambda(
     end
 
     maxλ = b
-    return maxλ, iter 
+    println("Σ_b=", Σ_b)
 
+    return maxλ, iter
 end
